a6f45429d69bbe24ae65a8de19ef19c6548895d2
Rohit Prasad <pdrohit674@gmail.com>
2026-06-11 01:33:48 +0530

    day 4 assignments added

---
+++ Assignments/Day4/day4-assignment.js
@@ -0,0 +1,11 @@
+let evenNumbers = [];
+let oddNumbers = [];
+
+for(let i = 1; i <= 100; i++) {
+    if(i % 2 === 0) evenNumbers.push(i);
+    else oddNumbers.push(i);
+}
+
+console.log("Even Numbers from 1 to 100 are " + evenNumbers);
+console.log("Odd Numbers from 1 to 100 are " + oddNumbers);
+
